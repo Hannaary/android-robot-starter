6ce416784370de9ccd4b72dba1591d20e61e3146
Anna Baskakova <146026108+Hannaary@users.noreply.github.com>
2025-07-21 00:47:59 +0700

    create multi-module maven project

---
+++ prototype/src/main/java/ru/kelf54/prototype/PrototypeApplication.java
@@ -0,0 +1,13 @@
+package ru.kelf54.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeApplication.class, args);
+    }
+
+}
